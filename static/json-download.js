b605038d915b7fd7a9883f7f4c36909f9e46b10e
Aya Abdellatif <ayaabdllatif112@gmail.com>
2025-10-04 02:24:44 +0300

    Add JSON download

---
+++ static/json-download.js
@@ -0,0 +1,15 @@
+
+  function downloadJSON() {
+    const data = {{ data|tojson }};   // your dataset
+    const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "weather_data.json";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
+  }
